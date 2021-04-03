9bc40f683581a2e8a2c8071081c4de317fc9b8bb
knyaazev <knyazevilya03@gmail.com>
2021-04-03 16:43:09 +0300

    Initial Commit

---
+++ lesson4practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson4practice
+//
+//  Created by admin2 on 03.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
